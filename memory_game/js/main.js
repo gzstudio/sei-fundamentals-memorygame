1c626ba221f5c6df17e436c1a3c060dc2e9f20a6
YR <yui.rasananda@gmail.com>
2019-07-21 13:29:55 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,21 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
+function checkForMatch(){
+	if (cardsInPlay.length === 2) {
+			if (cardsInPlay[0] === cardsInPlay[1]) {
+				alert("You found a match!");
+			}  else {
+				alert("Sorry, try again");
+			}
+		}
+};
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
+function flipCard(cardId){
+	console.log("User flipped "+ cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+};
 
-console.log("User flipped "+ cardsInPlay[0]);
-console.log("Uesr flipped "+ cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	}  else {
-		alert("Sorry, try again");
-	}
-}
+flipCard(0);
+flipCard(2);
